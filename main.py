65706b3f52789a6e292006e28a8692f1a270d00d
Ming Chen <mingc.mingc@gmail.com>
2021-10-06 17:06:41 +1300

    wip

--- main.py
+++ main.py
@@ -47,42 +47,37 @@ def format_datetime_to_str(d: datetime) -> str:
     return d.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]  # Remove last 3 digits
 
 
-def get_last_day_timestamp() -> str:
+def format_time_stamp(days_from_now=0, hours_from_now=0) -> str:
     finish = datetime.now(timezone.utc)
-    d = timedelta(days=1)
+    # finish = datetime.timestamp()
+    d = timedelta(days=days_from_now, hours=hours_from_now)
+    offset = timedelta(days=3)
     start = finish - d
-    formatted = f"timestamp:[{format_datetime_to_str(start)} TO {format_datetime_to_str(finish)}]"
+    formatted = f"timestamp:[{format_datetime_to_str(start - offset)} TO {format_datetime_to_str(finish - offset)}] AND "
     return formatted
 
 
-def build_query_string(q: str) -> str:
-    """Searches for the past 2 days"""
-    return f"{get_last_day_timestamp()} AND {q}"
-
-
-def collars_checked_into_wifi():
-    raw_query = 'serialNumber:004\-00\* AND context.farmId:"4ef316f1-4628-4457-9468-1a294d1e7769" AND _exists_:context.cattleId AND "WIFI_CHECKIN"'
-    query = build_query_string(raw_query)
+def collars_checked_into_wifi_last_day():
+    timestamp = format_time_stamp(days_from_now=1)
+    print(timestamp)
+    query = f'{timestamp}serialNumber:004\-00\* AND context.farmId:"4ef316f1-4628-4457-9468-1a294d1e7769" AND _exists_:context.cattleId AND "WIFI_CHECKIN"'
     return query_elastic_search(query)
 
 
-def collars_checked_into_lora():
-    raw_query = 'serialNumber:004\-00\* AND context.farmId:"4ef316f1-4628-4457-9468-1a294d1e7769" AND _exists_:context.cattleId AND "LORA_CHECKIN"'
-    query = build_query_string(raw_query)
+def collars_checked_into_lora_last_hour():
+    timestamp = format_time_stamp(hours_from_now=1)
+    query = f'{timestamp}serialNumber:004\-00\* AND context.farmId:"4ef316f1-4628-4457-9468-1a294d1e7769" AND _exists_:context.cattleId AND "LORA_CHECKIN"'
     return query_elastic_search(query)
 
 
 if __name__ == "__main__":
-    # es_query = f"serialNumber:004\-00\* AND metricName:OTA_EVENT "
-    # es_query = f"timestamp:[2021-10-04T00:00:00.000 TO 2021-10-04T23:59:59.000] AND serialNumber:004\-00\* AND metricName:OTA_EVENT "
-
     list_wifi_collars = set()
-    wifi_collars = collars_checked_into_wifi()
+    wifi_collars = collars_checked_into_wifi_last_day()
     for wifi_collar in wifi_collars:
         list_wifi_collars.add(wifi_collar['data']['serialNumber'])
 
     list_lora_collars = set()
-    lora_collars = collars_checked_into_lora()
+    lora_collars = collars_checked_into_lora_last_hour()
     for lora_collar in lora_collars:
         list_lora_collars.add(lora_collar['data']['serialNumber'])
 
